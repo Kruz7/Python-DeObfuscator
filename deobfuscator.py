cdc3d4c3b44c9e12cbd0b1047546f64eb975d154
Kruz <135480840+Kruz7@users.noreply.github.com>
2026-04-08 19:51:27 +0300

    Add files via upload

---
+++ deobfuscator.py
@@ -0,0 +1,132 @@
+import re
+import zlib
+import base64
+
+class detector:
+    def detect_layer(content):
+        try:
+            content = first_layer.deobfuscate_first_layer(content)
+            try:
+                content = second_layer.deobfuscate_second_layer(content)
+                content = third_layer.deobfuscate_third_layer(content)
+            except:
+                content = third_layer.deobfuscate_third_layer(content)
+                content = second_layer.deobfuscate_second_layer(content)
+        except:
+            try:
+                content = second_layer.deobfuscate_second_layer(content)
+                try:
+                    content = third_layer.deobfuscate_third_layer(content)
+                    content = first_layer.deobfuscate_first_layer(content)
+                except:
+                    content = first_layer.deobfuscate_first_layer(content)
+                    content = third_layer.deobfuscate_third_layer(content)
+            except:
+                content = third_layer.deobfuscate_third_layer(content)
+                try:
+                    content = second_layer.deobfuscate_second_layer(content)
+                    content = first_layer.deobfuscate_first_layer(content)
+                except:
+                    content = first_layer.deobfuscate_first_layer(content)
+                    content = second_layer.deobfuscate_second_layer(content)
+        with open("output.py", "w", encoding="utf-8") as f:
+            f.write(content)
+
+class first_layer:
+    def get_variables_name_and_values(content):
+
+        base64_values = []
+        matches = re.findall(r'\b([\w\u00C0-\u024F\u1E00-\u1EFF]+)\s*=\s*(.+?)\s*(?:$|(?=\n))', content, re.MULTILINE)
+        for match in matches:
+            value = match[1]
+            base64_values.append(eval(value))
+        return base64_values
+    
+    def remove_first_layer(base64_values):
+
+        base64_string = ""
+        for value in base64_values:
+            base64_string+=value
+        removed_first_layer = zlib.decompress(base64.b64decode(base64_string)).decode()
+        return removed_first_layer
+    
+    def deobfuscate_first_layer(content):
+        base64_values = first_layer.get_variables_name_and_values(content)
+        content = first_layer.remove_first_layer(base64_values)
+        return content
+
+class second_layer:
+    def get_variable_name_and_value(content):
+
+        match = re.search(r'\b([\w\u00C0-\u024F\u1E00-\u1EFF]+)\s*=\s*(.+?)\s*(?:$|(?=\n))', content, re.MULTILINE)
+        name_encrypted = match.group(1)
+        value = eval(match.group(2))
+        return name_encrypted, value
+
+    def get_in_loc(name_encrypted, content):
+
+        pattern = r'if\s+' + re.escape(name_encrypted) + r'\s*\[(.*?)\]'
+        match = re.search(pattern, content)
+        in_loc = match.group(1)
+        return eval(in_loc)
+
+    def get_re_loc(name_encrypted, content):
+
+        pattern = r'\b' + re.escape(name_encrypted) + r'\s*\[(.*?)\]'
+        matches = re.findall(pattern, content, re.MULTILINE)
+        re_loc = eval(matches[1])
+        return re_loc
+    
+    def get_i_value(value, re_loc, in_loc):
+
+        i_value = value[re_loc] ^ value[in_loc]
+        return i_value
+    
+    def remove_second_layer(i_value, value, in_loc, re_loc):
+
+        removed_second_layer = zlib.decompress(bytes(map(lambda x: x^i_value, value[0: in_loc] + value[in_loc + 1: re_loc] + value[re_loc + 1:]))).decode()
+        return removed_second_layer
+    
+    def deobfuscate_second_layer(content):
+        name_encrypted, value = second_layer.get_variable_name_and_value(content)
+        in_loc = second_layer.get_in_loc(name_encrypted, content)
+        re_loc = second_layer.get_re_loc(name_encrypted, content)
+        i_value = second_layer.get_i_value(value, re_loc, in_loc)
+
+        content = second_layer.remove_second_layer(i_value, value, in_loc, re_loc)
+
+        return content
+    
+class third_layer:
+    def get_ip_table(content):
+
+        match = re.search(r'\b([\w\u00C0-\u024F\u1E00-\u1EFF\u4E00-\u9FFF]+)\s*=\s*(.+?)\s*(?:$|(?=\n))', content, re.MULTILINE)
+        ip_table = eval(match.group(2))
+        return ip_table
+    
+    def get_data(ip_table):
+        data = list([int(x) for item in [value.split(".") for value in ip_table] for x in item])
+        return data
+    
+    def remove_third_layer(data):
+
+        third_layer_removed = zlib.decompress(base64.b64decode(bytes(data))).decode()
+        return third_layer_removed
+    
+    def deobfuscate_third_layer(content):
+        ip_table = third_layer.get_ip_table(content)
+        data = third_layer.get_data(ip_table)
+        content = third_layer.remove_third_layer(data)
+        return content
+    
+def main():
+
+    file = input("insert the file path:")
+
+    with open(file,"r", encoding="utf-8") as f:
+        content = f.read()
+
+    detector.detect_layer(content)
+
+if __name__ == "__main__":
+    main()
